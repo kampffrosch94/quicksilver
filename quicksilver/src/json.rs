1a99bad00ceb49c8a589d8a20bd74ed401e84e7b
kampffrosch94 <kampffrosch94@github.com>
2025-05-25 11:48:28 +0200

    use derive macro

--- quicksilver/src/json.rs
+++ quicksilver/src/json.rs
@@ -131,73 +131,19 @@ mod test {
     use std::mem;
 
     use crate::{json::from_json, vec::VecVtableCreator, *};
-    #[derive(Debug, PartialEq)]
+    #[derive(Debug, PartialEq, Quicksilver)]
     struct Point {
         x: i32,
         y: i32,
     }
 
-    impl Reflection for Point {
-        const MIRROR: &'static Struct = &Struct {
-            name: "Point",
-            size: size_of::<Self>(),
-            align: align_of::<Self>(),
-            fields: &[
-                Field {
-                    name: "x",
-                    ty: Type::I32,
-                    offset: mem::offset_of!(Self, x),
-                },
-                Field {
-                    name: "y",
-                    ty: Type::I32,
-                    offset: mem::offset_of!(Self, y),
-                },
-            ],
-        };
-    }
-    #[derive(Debug, PartialEq)]
+    #[derive(Debug, PartialEq, Quicksilver)]
     struct MyData {
         id: u32,
         name: String,
         value: f32,
         location: Point,
         is_active: i32, // Using i32 to demonstrate another integer type
-    }
-
-    impl Reflection for MyData {
-        const MIRROR: &'static Struct = &Struct {
-            name: "MyData",
-            size: size_of::<Self>(),
-            align: align_of::<Self>(),
-            fields: &[
-                Field {
-                    name: "id",
-                    ty: Type::U32,
-                    offset: mem::offset_of!(MyData, id),
-                },
-                Field {
-                    name: "name",
-                    ty: Type::String,
-                    offset: mem::offset_of!(MyData, name),
-                },
-                Field {
-                    name: "value",
-                    ty: Type::F32,
-                    offset: mem::offset_of!(MyData, value),
-                },
-                Field {
-                    name: "location",
-                    ty: Type::Struct(Point::MIRROR),
-                    offset: mem::offset_of!(MyData, location),
-                },
-                Field {
-                    name: "is_active",
-                    ty: Type::I32,
-                    offset: mem::offset_of!(MyData, is_active),
-                },
-            ],
-        };
     }
 
     #[test]
--- quicksilver/src/lib.rs
+++ quicksilver/src/lib.rs
@@ -130,74 +130,19 @@ mod tests {
     use super::*;
 
     use std::mem;
-    #[derive(Debug)]
+    #[derive(Debug, Quicksilver)]
     struct Point {
         x: i32,
         y: i32,
     }
 
-    impl Reflection for Point {
-        const MIRROR: &'static Struct = &Struct {
-            name: "Point",
-            size: size_of::<Self>(),
-            align: align_of::<Self>(),
-            fields: &[
-                Field {
-                    name: "x",
-                    ty: Type::I32,
-                    offset: mem::offset_of!(Point, x),
-                },
-                Field {
-                    name: "y",
-                    ty: Type::I32,
-                    offset: mem::offset_of!(Point, y),
-                },
-            ],
-        };
-    }
-
-    #[derive(Debug)]
+    #[derive(Debug, Quicksilver)]
     struct MyData {
         id: u32,
         name: String,
         value: f32,
         location: Point,
         is_active: i32, // Using i32 to demonstrate another integer type
-    }
-
-    impl Reflection for MyData {
-        const MIRROR: &'static Struct = &Struct {
-            name: "MyData",
-            size: size_of::<Self>(),
-            align: align_of::<Self>(),
-            fields: &[
-                Field {
-                    name: "id",
-                    ty: Type::U32,
-                    offset: mem::offset_of!(MyData, id),
-                },
-                Field {
-                    name: "name",
-                    ty: Type::String,
-                    offset: mem::offset_of!(MyData, name),
-                },
-                Field {
-                    name: "value",
-                    ty: Type::F32,
-                    offset: mem::offset_of!(MyData, value),
-                },
-                Field {
-                    name: "location",
-                    ty: Type::Struct(Point::MIRROR),
-                    offset: mem::offset_of!(MyData, location),
-                },
-                Field {
-                    name: "is_active",
-                    ty: Type::I32,
-                    offset: mem::offset_of!(MyData, is_active),
-                },
-            ],
-        };
     }
 
     #[test]
