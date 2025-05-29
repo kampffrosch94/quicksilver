303a1afb36dcbf66fcd88eeaeae7805b45977267
kampffrosch94 <kampffrosch94@github.com>
2025-05-29 10:08:53 +0200

    hashmap WIP

--- quicksilver/src/map.rs
+++ quicksilver/src/map.rs
@@ -1,15 +1,20 @@
 use std::{collections::HashMap, marker::PhantomData};
 
-use crate::{FieldReflection, Reflection, StructReflection, Type, ValueReflection, reflect_value};
+use crate::{
+    FieldReflection, Reflectable, Reflection, StructReflection, Type, ValueReflection,
+    reflect_value,
+};
 use std::hash::Hash;
 
 #[derive(Debug)]
 pub struct HMVtable {
     /// creates the HashMap of current Type at the pointer coordinate
-    /// returns pointer to the first element
     pub new_at: unsafe fn(ptr: *mut u8),
+    /// adds elements to hashmap
+    /// elements needs to be a pointer to a `Vec<HashMapEntry<K,V>>`
     pub fill_with: unsafe fn(ptr: *mut u8, elements: *mut u8),
-    pub get_elements: unsafe fn(ptr: *mut u8) -> *mut u8,
+    /// returns all elements in HashMap in whatever iteration order it sees fit
+    pub get_elements: unsafe fn(ptr: *mut u8) -> Vec<StructReflection<'static>>,
 }
 
 pub struct HMEntry<Key, Value> {
@@ -24,21 +29,27 @@ pub struct HMEntryView<Key, Value> {
     value: *mut Value,
 }
 
-impl<Key, Value> HMEntryView<Key, Value> {
+impl<Key, Value> HMEntryView<Key, Value>
+where
+    Key: Reflectable,
+    Value: Reflectable,
+{
     /// unsafe because caller needs to uphold lifetime invariant
     unsafe fn reflect(self) -> StructReflection<'static> {
         StructReflection {
             name: "HMEntry",
-            fields: vec![
-                FieldReflection {
-                    name: "key",
-                    value: todo!(),
-                },
-                FieldReflection {
-                    name: "value",
-                    value: todo!(),
-                },
-            ],
+            fields: unsafe {
+                vec![
+                    FieldReflection {
+                        name: "key",
+                        value: reflect_value(self.key as *mut u8, &Key::TYPE),
+                    },
+                    FieldReflection {
+                        name: "value",
+                        value: reflect_value(self.value as *mut u8, &Key::TYPE),
+                    },
+                ]
+            },
         }
     }
 }
@@ -51,6 +62,8 @@ impl<Key, Value> HMVtableCreator<Key, Value>
 where
     Key: Eq,
     Key: Hash,
+    Key: Reflection,
+    Value: Reflection,
 {
     pub const VTABLE: HMVtable = HMVtable {
         new_at: Self::new_at,
@@ -78,9 +91,9 @@ where
         }
     }
 
-    unsafe fn get_elements(ptr: *mut u8) -> *mut u8 {
+    unsafe fn get_elements(ptr: *mut u8) -> Vec<StructReflection<'static>> {
         let ptr = ptr as *mut HashMap<Key, Value>;
-        let mut result: Vec<HMEntryView<Key, Value>> = Vec::new();
+        let mut result = Vec::new();
         unsafe {
             let val = &mut *ptr;
             for (key, value) in val.iter_mut() {
@@ -88,10 +101,9 @@ where
                     key: key as *const Key,
                     value: value as *mut Value,
                 };
+                result.push(el.reflect());
             }
         }
-        // TODO: how to get values out in a type erased manner
-        // maybe use a Reflected Value for the Vec?
-        todo!()
+        result
     }
 }
